13d57da8065feec39f8a89ef2861a18f879b41fc
nirmalrepo <w.v.nirmalfernando@gmail.com>
2024-02-18 11:48:05 +1300

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
+import { draftMode } from "next/headers";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import LiveVisualEditing from "@/components/LiveVisualEditing";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {draftMode().isEnabled && <LiveVisualEditing />}
+      </body>
     </html>
   );
 }
